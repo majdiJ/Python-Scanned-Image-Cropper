0da7d48fa3257aa7aaa8333e2065f67c9730a492
Majdi Jaigirdar <majdijaigirdar@mac.lan>
2025-01-07 12:41:12 +0000

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Python Scanned Image Cropper
+# This program is designed to crop scanned images saved as jpg/jpeg of photos, crop them and save them in a new folder.
+
+import os
+
+def initalise_folders():
+    # Specify the input and output folders
+    input_folder = 'input_images'
+    output_folder = 'output_images_cropped'
+
+    # Ensure both the folders exist, if they don't create them
+
+main()
